2b3585f7fdfc6cd21746287319a26af60fa70a38
Tatyana Prohorova <tatyanaprohorova@gmail.com>
2022-11-15 13:47:14 +0300

    Initial. Webpack started

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        // filename: 'main.js',
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'main.html'
+    }),
+    new MiniCSSExtractPlugin()
+    ],
+    module: {
+        rules:[
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.css$/, 
+                    use: [
+                        MiniCSSExtractPlugin.loader,
+                        'css-loader'
+                    ],
+                },
+        ]},
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         template: './index.html',
+    //         filename: 'main.html'
+    //     }),
+    //     new MiniCSSExtractPlugin()
+    // ]
+}
